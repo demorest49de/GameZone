fe82e7bbeafa9c3fed56da4bd63d51d38b214b46
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-29 19:43:27 +0300

    Changes call-modal.js changed;

--- js/modules/call-modal.js
+++ js/modules/call-modal.js
@@ -1,6 +1,7 @@
-const modal = document.querySelector('.header__call-button');
+const modalBtn = document.querySelector('.header__call-button');
 const callOverlay = document.querySelector('.call-overlay');
 const body = document.body;
+const callFrom = callOverlay.querySelector('.call__form');
 
 callOverlay.style.display = 'block';
 
@@ -8,7 +9,7 @@ const toggleModalVisible = (isActive) => {
   body.style.overflow = isActive ? 'hidden' : 'visible';
 };
 
-modal.addEventListener("click", () => {
+modalBtn.addEventListener("click", () => {
   const isActive = callOverlay.classList.toggle('is-visible');
   toggleModalVisible(isActive && screen.width > 768);
 });
@@ -24,3 +25,11 @@ window.addEventListener('resize', () => {
   toggleModalVisible(callOverlay.classList.contains('is-visible') && screen.width > 768);
   // console.log(' : ', screen.width, callOverlay.classList.contains('is-visible'));
 });
+
+// callFrom.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const formData = new FormData(e.target);
+//   const data = Object.fromEntries(formData);
+//
+//
+// });
