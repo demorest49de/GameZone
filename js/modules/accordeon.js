4316fa947f0569c9ebf13d724cc29f3dc38ca341
Shevchenko Andrey <demorest49de@gmail.com>
2023-03-31 21:36:50 +0300

    Changes accordeon.js changed; faq.css changed; index.html changed;

--- js/modules/accordeon.js
+++ js/modules/accordeon.js
@@ -1,26 +1,29 @@
 const list = document.querySelector('.faq__list');
 const itemActive = document.getElementsByClassName('faq__item_active');
-const txtWrappers = document.querySelectorAll('.faq__text-wrapper');
+const textWrapper = document.querySelectorAll('.faq__item-wrapper');
 let heightWrapper = 0;
 
-txtWrappers.forEach(item => {
+textWrapper.forEach(item => {
   if (item.scrollHeight > heightWrapper) {
     heightWrapper = item.scrollHeight;
   }
 });
 
+console.log(' : ', heightWrapper);
+
 list.addEventListener('click', ({target}) => {
+
   if (itemActive[0] && itemActive[0] !== target.closest('.faq__item')) {
-    itemActive[0].querySelector('.faq__text-wrapper').style.height = '0';
+    itemActive[0].querySelector('.faq__item-wrapper').style.height = '0';
 
     itemActive[0].classList.remove('faq__item_active');
   }
+
   if (target.closest('.faq__button')) {
+
     const item = target.closest('.faq__item');
     const isActive = item.classList.toggle('faq__item_active');
-
-    const txtWrapper = item.querySelector('.faq__text-wrapper');
+    const txtWrapper = item.querySelector('.faq__item-wrapper');
     txtWrapper.style.height = isActive ? `${heightWrapper}px` : '0';
-
   }
 });
