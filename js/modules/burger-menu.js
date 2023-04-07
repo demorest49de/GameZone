e029f7fcd9c4c3b200b0e38e0ca321ad67c7abdd
Shevchenko Andrey <demorest49de@gmail.com>
2023-04-07 13:48:27 +0300

    Changes .gitignore changed; burger.css changed; burger-menu.js changed; index.html changed; temp.js added;

--- js/modules/burger-menu.js
+++ js/modules/burger-menu.js
@@ -1,4 +1,8 @@
 import {toggleModalVisible} from './call-modal.js';
 
-
-
+const burgerBtn = document.querySelector('.header__navigation');
+const burger = document.querySelector('.burger');
+const showMenu = () => {
+  burger.classList.toggle('burger-visible');
+};
+burgerBtn.addEventListener('click', showMenu);
