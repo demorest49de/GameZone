c93c988c69682e9458b1864d13114bd292c07787
Shevchenko Andrey <demorest49de@gmail.com>
2023-04-18 13:01:31 +0300

    Changes burger.css changed; burgerMenu.js changed;

--- js/modules/burgerMenu.js
+++ js/modules/burgerMenu.js
@@ -4,38 +4,46 @@ export const burgerHandler = () => {
   const burgerOverlay = document.querySelector('.burger');
   const callBtn = document.querySelector('.header__call-button');
   const header = document.querySelector('.header');
-  const burgerVisible = document.querySelector('.burger-visible');
+  // const burgerVisible = document.querySelector('.burger-visible');
 
   let opacity = 0;
+  let visibility = false;
+  burgerOverlay.style.opacity = opacity;
+  burgerOverlay.style.visibility = `hidden`;
 
-  burgerVisible.style.cssText = `
-    opacity: ${opacity};
-    visibility: visible;
-  `;
+  const changeVisibility = () => {
+    visibility = !visibility;
+    burgerOverlay.style.visibility = `${visibility ? 'visible' : 'hidden'}`;
+    burgerOverlay.style.transition = `all 0.5s ease-in-out`;
+  };
 
-
+  const toggleHandler = () => {
+    if (visibility && opacity < 1) {
+      opacity += 0.03;
+      burgerOverlay.style.opacity = opacity;
+      requestAnimationFrame(toggleHandler);
+    } else if (!visibility && opacity > 0) {
+      opacity -= 0.03;
+      burgerOverlay.style.opacity = opacity;
+      requestAnimationFrame(toggleHandler);
+    }
+  };
 
   const toggleMenu = () => {
-    const visible = burgerOverlay.classList.toggle('burger-visible');
-
-    if(visible){
-      opacity += 0.03;
-      burgerVisible
-    }
-
+    changeVisibility();
+    toggleHandler();
     isBurgeMenuVisible();
   };
 
   const isBurgeMenuVisible = () => {
 
-    const visible = burgerOverlay.classList.contains('burger-visible');
     burgerBtn.style.backgroundSize = `contain`;
     burgerBtn.style.backgroundRepeat = `no-repeat`;
     burgerBtn.style.backgroundPosition = `center`;
 
     burgerBtn.style.transition = `all .3s ease-in-out`;
 
-    if (visible) {
+    if (visibility) {
       burgerBtn.style.backgroundImage = `url(../img/header/close.svg)`;
     } else {
       burgerBtn.style.backgroundImage = `url(../img/header/menu.svg)`;
